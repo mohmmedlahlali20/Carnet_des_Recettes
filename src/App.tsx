970d8793e0eca2f1c0abd08c501aec7f028e102c
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-03-13 23:12:12 +0000

    feat(app): update App component for improved functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import AppNavigator from "./components/AppNavigator.tsx";
+import {Provider} from "react-redux";
+import store from "./redux/store/store.ts";
 
 export default function App() {
-    return <AppNavigator />;
+    return (
+
+        <Provider store={store}>
+            <AppNavigator />
+        </Provider>
+
+    );
 }
