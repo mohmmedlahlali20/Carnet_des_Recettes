a42e4c784c82caa4cf7068f17b5195a123118ac6
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-03-14 10:27:26 +0000

    refactor(navigation): update AppNavigator with authentication flow

--- src/components/AppNavigator.tsx
+++ src/components/AppNavigator.tsx
@@ -1,18 +1,53 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-import HomeScreen from '../screens/HomeScreen';
+import React, { useEffect } from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
+
+import HomeScreen from "../screens/HomeScreen";
 import RecipeDetailsScreen from '../screens/RecipeDetailsScreen';
+import RegisterScreen from "../screens/RegisterScreen";
+import {useAppSelector} from "../hooks/useAppDispatch.ts";
 
 const Stack = createStackNavigator();
+
+const MainNavigator = () => {
+  // @ts-ignore
+    const {isAuthenticated} = useAppSelector(state => state.auth);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+      console.log(isAuthenticated);
+    if (!isAuthenticated) {
+      // @ts-ignore
+      navigation.navigate('Register');
+    }
+  }, [isAuthenticated, navigation]);
+
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{title: 'Recettes'}}
+      />
+      <Stack.Screen
+        name="RecipeDetails"
+        component={RecipeDetailsScreen}
+        options={{title: 'Détails'}}
+      />
+      <Stack.Screen
+        name="Register"
+        component={RegisterScreen}
+        options={{title: 'Inscription'}}
+      />
+    </Stack.Navigator>
+  );
+};
 
 export default function AppNavigator() {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Recettes' }} />
-                <Stack.Screen name="RecipeDetails" component={RecipeDetailsScreen} options={{ title: 'Détails' }} />
-            </Stack.Navigator>
+            <MainNavigator />
         </NavigationContainer>
     );
 }
